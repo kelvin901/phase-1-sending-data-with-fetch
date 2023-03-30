dda69036d2b07e7d87e8699b888b44c79b8ea78a
kelvin901 <wawerukelvin2019@gmail.com>
2023-03-30 18:28:31 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+const BASE_URL = "http://localhost:3000/users";
+const body = document.querySelector('body');
+
+const submitData = (name, email) => {
+    fetch(BASE_URL, {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        }),
+    })
+    .then((response) => response.json())
+    .then((object) => {
+        const h1 = document.createElement('h1');
+        h1.textContent = object.id;
+        body.appendChild(h1);
+    })
+    .catch((error) => {
+        console.log(error);
+        document.body.innerHTML = error;
+    })
+};
+
+submitData('Kelvin', 'kelvin.nyoike@student.moringaschool.com');
